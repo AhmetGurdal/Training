5d2e63a9955800e6cb459f47f7e15cc6db5672dc
Azaraks-AG <ahmetgurdal32@gmail.com>
2018-02-21 17:37:50 +0300

    Training

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a date dd/mm/yyyy");
+            string sdate = Console.ReadLine();
+            string[] spdate = sdate.Split("/");
+            DateTime date = new DateTime(int.Parse(spdate[2]),int.Parse(spdate[1]),int.Parse(spdate[0]));
+            Console.Write(date);
+        }
+    }
+}
